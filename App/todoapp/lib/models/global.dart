e8bca747ef07d0fa70158464f4cf078250385c14
Igor Silchenko <kildsforkids@gmail.com>
2020-04-06 19:33:52 +1000

    Create intray top bar and button #2

---
+++ App/todoapp/lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF3A3A3A);
+Color violetColor = new Color(0xFFA26CFF);
+
+TextStyle intrayTitleStyle = new TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 48);
